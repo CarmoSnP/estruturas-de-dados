025e84c81d7fd2e2a4f6ae248ad5b0878aea7852
Igor do Carmo <111009962+CarmoSnP@users.noreply.github.com>
2025-02-21 15:26:45 +0000

    commit com todas as funcoes do doublelist

--- exercise/doubly_linked_list.cpp
+++ exercise/doubly_linked_list.cpp
@@ -1,54 +1,76 @@
 #include "../include/doubly_linked_list.hpp"
 #include <iostream>
 
-int main()
-{
-  DoublyLinkedList<int> lista;
+int main() {
+    DoublyLinkedList<int> lista;
 
-  lista.push_front(10);
-  lista.push_front(20);
-  lista.push_front(30);
+    std::cout << "Inserindo elementos no início e no fim da lista...\n";
+    lista.push_front(10);
+    lista.push_front(20);
+    lista.push_front(30);
 
-  lista.push_back(5);
-  lista.push_back(6);
-  lista.push_back(7);
+    lista.push_back(5);
+    lista.push_back(6);
+    lista.push_back(7);
 
-  std::cout << "removendo o primeiro da lista" << std::endl;
-  lista.pop_front();
-  std::cout << "removendo o primeiro da lista" << std::endl;
-  lista.pop_front();
+    lista.print();
 
-  lista.print();
-  std::cout << "removendo o último da lista" << std::endl;
-  lista.pop_back();
+    std::cout << "Removendo o primeiro elemento...\n";
+    lista.pop_front();
+    lista.print();
 
-  lista.print();
+    std::cout << "Removendo o último elemento...\n";
+    lista.pop_back();
+    lista.print();
 
-  for (auto &i : lista) 
-  {
-    std::cout << i << ",";
-  }
-  std::cout << '\n';
+    std::cout << "Iterando sobre os elementos da lista:\n";
+    for (auto &i : lista) {
+        std::cout << i << " ";
+    }
+    std::cout << '\n';
 
-  lista.insert(lista.begin(), 15);
+    std::cout << "Inserindo 15 no início da lista usando insert...\n";
+    lista.insert(lista.begin(), 15);
+    lista.print();
 
-  lista.print();
+    std::cout << "Acessando elementos por índice usando operator[]:\n";
+    for (size_t i = 0; i < lista.size(); ++i) {
+        std::cout << "lista[" << i << "] = " << lista[i] << '\n';
+    }
 
-  lista[4];
-  lista[5];
+    std::cout << "Testando operador de iteração (++ e --):\n";
+    auto pos = lista.begin();
+    std::cout << "Primeiro da lista = " << *pos << '\n';
+    ++pos;
+    std::cout << "Segundo da lista = " << *pos << '\n';
+    --pos;
+    std::cout << "Voltando para o primeiro da lista = " << *pos << '\n';
 
-  auto pos = lista.begin();
-  std::cout<<"Primeiro da lista = "<<*pos<<'\n';
-  ++pos;
-  std::cout<<"Segundo da lista = "<<*pos<<'\n';
-  --pos;
-  std::cout<<"Primeiro da lista = "<<*pos<<'\n';
+    std::cout << "Inserindo 55 na posição 4...\n";
+    pos = lista.begin() + 4;
+    lista.insert(pos, 55);
+    lista.print();
 
-  pos = lista.begin() + 4;
+    std::cout << "Testando find() e contains():\n";
+    int busca = 15;
+    if (lista.contains(busca)) {
+        std::cout << "O elemento " << busca << " está na lista.\n";
+    } else {
+        std::cout << "O elemento " << busca << " não foi encontrado.\n";
+    }
 
-  lista.insert(pos,55);
+    std::cout << "Removendo um intervalo de elementos...\n";
+    lista.erase(lista.begin(), lista.begin() + 2);
+    lista.print();
 
-  lista.print();
+    std::cout << "Limpando a lista completamente...\n";
+    lista.clear();
+    
+    if (lista.empty()) {
+        std::cout << "A lista está vazia!\n";
+    } else {
+        lista.print();
+    }
 
-  return 0;
+    return 0;
 }
